5f3dbf244be37923d584bf74122a885aafdf4546
Dima Koltovich <steklopod@gmail.com>
2018-10-04 11:13:39 +0300

    PrintMyActorRefActor class added

---
+++ src/main/scala/ru/sample/PrintMyActorRefActor.scala
@@ -0,0 +1,25 @@
+package ru.sample
+
+
+import akka.actor.{ Actor, Props, ActorSystem }
+import scala.io.StdIn
+
+class PrintMyActorRefActor extends Actor {
+  override def receive: Receive = {
+    case "printit" ⇒
+      val secondRef = context.actorOf(Props.empty, "second-actor")
+      println(s"Second: $secondRef")
+  }
+}
+
+object ActorHierarchyExperiments extends App {
+  val system = ActorSystem("testSystem")
+
+  val firstRef = system.actorOf(Props[PrintMyActorRefActor], "first-actor")
+  println(s"First: $firstRef")
+  firstRef ! "printit"
+
+  println(">>> Press ENTER to exit <<<")
+  try StdIn.readLine()
+  finally system.terminate()
+}
