c19d0cc24ab0336d65695899cdd73a9d336ba2f9
steklopod <steklopod@gmail.com>
2018-10-09 19:18:17 +0300

    Packt example added (as .rar in resources)

---
+++ src/main/scala/ru/packt/o3/actor_path/App.scala
@@ -0,0 +1,16 @@
+package ru.packt.o3.actor_path
+
+import akka.actor.{ActorSystem, Props}
+
+object Watch extends App {
+
+  val system = ActorSystem("Watsh-actor-selection")
+
+  val counter = system.actorOf(Props[Counter], "counter")
+
+  val watcher = system.actorOf(Props[Watcher], "watcher")
+
+  Thread.sleep(1000)
+
+  system.terminate()
+}
